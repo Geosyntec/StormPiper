cfacf263d2d6ec439cacf0dd1a995af54ec9878d
Austin Orr <aorr@geosyntec.com>
2022-08-01 17:09:00 -0700

    robustify login

--- stormpiper/stormpiper/earth_engine/login.py
+++ stormpiper/stormpiper/earth_engine/login.py
@@ -3,6 +3,11 @@ import base64
 import logging
 
 import ee
+from tenacity import after_log  # type: ignore
+from tenacity import before_log  # type: ignore
+from tenacity import stop_after_attempt  # type: ignore
+from tenacity import wait_fixed  # type: ignore
+from tenacity import retry
 
 from stormpiper.core.config import settings
 
@@ -10,6 +15,12 @@ logging.basicConfig(level=settings.LOGLEVEL)
 logger = logging.getLogger(__name__)
 
 
+@retry(
+    stop=stop_after_attempt(60 * 5),  # 10 mins
+    wait=wait_fixed(2),  # 2 seconds
+    before=before_log(logger, logging.INFO),
+    after=after_log(logger, logging.WARN),
+)
 def login(**kwargs):
     email = kwargs.pop("email", settings.EE_SERVICE_ACCOUNT)
     key_file = kwargs.pop("key_file", None)
@@ -21,11 +32,10 @@ def login(**kwargs):
             email=email, key_file=key_file, key_data=key_data
         )
         ee.Initialize(credentials)
+        return True
     except Exception as e:
         logger.exception("Error logging in to earth engine", e, exc_info=True)
-        return False
-
-    return True
+        raise e
 
 
 async def ee_continuous_login(sleep_seconds: int = 3600) -> None:
