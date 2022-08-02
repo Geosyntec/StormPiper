cf94a9cfd032c252985683daedf01b4970375b21
Austin Orr <aorr@geosyntec.com>
2022-08-01 17:20:03 -0700

    refactor bg tasks

--- stormpiper/stormpiper/api/endpoints/bg_worker.py
+++ stormpiper/stormpiper/api/endpoints/bg_worker.py
@@ -1,17 +1,12 @@
-import json
 from enum import Enum
 from typing import Any, Dict
 
-import geopandas
-import numpy
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.responses import JSONResponse
 
 import stormpiper.bg_worker as bg
 from stormpiper.apps.supersafe.users import check_admin
 from stormpiper.core import utils
-from stormpiper.database.connection import engine
-from stormpiper.src.solve_structural_wq import solve_wq_epochs_from_db
 
 router = APIRouter(dependencies=[Depends(check_admin)])
 rpc_router = APIRouter(dependencies=[Depends(check_admin)])
@@ -31,21 +26,49 @@ async def get_task(task_id: str) -> Dict[str, Any]:
 
 
 # RPC Routes
+
+
 class StrEnum(str, Enum):
     ...
 
 
 # TaskName = create_model("TaskName", **{k: k for k in bg.celery_app.tasks.keys()}, __base__=StrEnum)
 _tasks = [k for k in sorted(bg.celery_app.tasks.keys()) if "bg_worker" in k]
-TaskName = StrEnum("TaskName", {k: k for k in _tasks})
+TaskName: StrEnum = StrEnum("TaskName", {k: k for k in _tasks})
 
 
-@rpc_router.get("/run_task", response_class=JSONResponse)
-async def run_task(taskname: TaskName) -> Dict[str, Any]:
+@rpc_router.get("/run_task/{taskname}", response_class=JSONResponse)
+async def run_task(
+    taskname: TaskName, timeout: float = Query(0.5, le=120)
+) -> Dict[str, Any]:
 
     task = bg.celery_app.send_task(taskname)
-    _ = await utils.wait_a_sec_and_see_if_we_can_return_some_data(task, timeout=0.5)
+    _ = await utils.wait_a_sec_and_see_if_we_can_return_some_data(task, timeout=timeout)
     response = dict(task_id=task.task_id, status=task.status)
+    if task.successful():
+        response["data"] = task.result
+
+    return response
+
+
+_tasks = sorted([k for k in bg.Workflows.__dict__.keys() if not k.startswith("_")])
+Workflows = StrEnum("Workflows", {k: k for k in _tasks})
+
+
+@rpc_router.get("/run_workflow/{taskname}", response_class=JSONResponse)
+async def run_workflow(
+    taskname: Workflows, timeout: float = Query(0.5, le=120)
+) -> Dict[str, Any]:
+
+    t = getattr(bg.Workflows, taskname, None)
+
+    if t is None:
+        raise HTTPException(status_code=404, detail=f"not found: {taskname}")
+
+    task = t.apply_async()
+
+    _ = await utils.wait_a_sec_and_see_if_we_can_return_some_data(task, timeout=timeout)
+    response = dict(task_id=task.id, status=task.status)
     if task.successful():
         response["data"] = task.result
 
@@ -63,54 +86,12 @@ async def ping_background() -> Dict[str, Any]:
     return response
 
 
-@rpc_router.get("/delete_and_refresh_tacoma_gis_tables", response_class=JSONResponse)
-async def delete_and_refresh_tacoma_gis_tables() -> Dict[str, Any]:
+@rpc_router.get("/solve_watershed", response_class=JSONResponse)
+async def solve_watershed() -> Dict[str, Any]:
 
-    task = bg.delete_and_refresh_tacoma_gis_tables.apply_async()
+    task = bg.Workflows.refresh_results.apply_async()
     response = dict(task_id=task.task_id, status=task.status)
     if task.successful():
         response["data"] = task.result
 
     return response
-
-
-@rpc_router.get("/overlay_rodeo", response_class=JSONResponse)
-async def overlay_rodeo() -> Dict[str, Any]:
-
-    with engine.begin() as conn:
-        delin = geopandas.read_postgis("tmnt_facility_delineation", con=conn)
-        subs = geopandas.read_postgis("subbasin", con=conn)
-        out = (
-            geopandas.overlay(delin, subs, how="union", keep_geom_type=True)
-            .assign(subbasin=lambda df: df["subbasin"].fillna("None").astype(str))
-            .assign(
-                node_id=lambda df: numpy.where(
-                    df["node_id"].isna(),
-                    "SB_" + df["subbasin"],
-                    df["node_id"] + "_SB_" + df["subbasin"],
-                )
-            )
-            .loc[lambda df: df.geometry.area > 1.0]
-            .reindex(
-                columns=[
-                    "node_id",
-                    "altid",
-                    "relid",
-                    "subbasin",
-                    "basinname",
-                    "geometry",
-                ]
-            )
-        )
-
-        json_str = out.pipe(utils.datetime_to_isoformat).to_crs(4326).to_json()
-
-        return json.loads(json_str)
-
-
-@rpc_router.get("/solve_watershed", response_class=JSONResponse)
-async def solve_watershed() -> Dict[str, Any]:
-
-    df = solve_wq_epochs_from_db()
-
-    return {"results": df.head(10).to_dict(orient="records")}
