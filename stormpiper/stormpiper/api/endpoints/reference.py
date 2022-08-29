c60699abc4c4f7a212b42aaf7e9f978c9384b77e
Austin Orr <aorr@geosyntec.com>
2022-08-29 12:49:07 -0700

    add connections verification route

--- stormpiper/stormpiper/api/endpoints/reference.py
+++ stormpiper/stormpiper/api/endpoints/reference.py
@@ -1,8 +1,9 @@
-import re
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import JSONResponse
+from sqlalchemy.engine.url import make_url
 
 from stormpiper.apps.supersafe.users import check_user, check_admin
+from stormpiper.core.config import settings
 from stormpiper.core.context import get_context
 
 router = APIRouter(dependencies=[Depends(check_user)])
@@ -14,19 +15,17 @@ async def get_cxt(context=Depends(get_context)):
 
 
 @router.get("/check_router", dependencies=[Depends(check_admin)], name="check_router")
-async def get_check(request: Request, context=Depends(get_context)):
+async def get_check(request: Request):
 
     rsp = {
         "request.url": str(request.url),
-        "request.scope.keys()": list(map(str, request.scope.keys())),
         "scheme": str(request.scope["scheme"]),
-        "root_path": str(request.scope["root_path"]),
-        "path": str(request.scope["path"]),
-        "raw_path": str(request.scope["raw_path"]),
         "request.url_for": request.url_for("check_router"),
         "request.scope['router'].url_path_for": request.scope["router"].url_path_for(
             "check_router"
         ),
+        "database_url_async": repr(make_url(settings.DATABASE_URL_ASYNC)),
+        "database_url_sync": repr(make_url(settings.DATABASE_URL_SYNC)),
     }
 
     return JSONResponse(content=rsp)
