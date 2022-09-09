f7ac393989e988dd8544f781644f71c3c3c79cb5
Austin Orr <aorr@geosyntec.com>
2022-09-09 16:39:36 -0700

    refactor email send function

--- stormpiper/stormpiper/apps/supersafe/email.py
+++ stormpiper/stormpiper/apps/supersafe/email.py
@@ -1,4 +1,5 @@
 import base64
+import logging
 
 import aiohttp
 
@@ -6,12 +7,16 @@ from stormpiper.core.config import settings
 
 from . import email_templates
 
+logging.basicConfig(level=settings.LOGLEVEL)
+logger = logging.getLogger(__name__)
+
 
 async def post(url, headers, json=None):
     async with aiohttp.ClientSession() as client:
         async with client.post(url, headers=headers, json=json) as resp:
-            print(await resp.text())
-            print(resp.status)
+            txt = await resp.text()
+            log = logging.warning if resp.status > 200 else logging.info
+            log(f"response status: {resp.status} text: {txt}")
 
 
 async def send_email_to_user(*, template: str, client=None, **template_kwargs):
@@ -29,45 +34,6 @@ async def send_email_to_user(*, template: str, client=None, **template_kwargs):
         await post(url, headers=headers, json=_json)
     else:
         async with client.post(url, headers=headers, json=_json) as resp:
-            print(await resp.text())
-            print(resp.status)
-
-
-# async def send_reset_password_token_to_user(
-#     *, email, token, name=None, client=None, reset_url=""
-# ):
-
-#     url = settings.EMAIL_SEND_URL
-#     key_secret = f"{settings.EMAIL_API_KEY}:{settings.EMAIL_API_SECRET}"
-#     auth = base64.b64encode(key_secret.encode()).decode()
-#     headers = {"Authorization": f"Basic {auth}"}
-
-#     _json = build_reset_token_json(email=email, token=token, name=name, url=reset_url)
-
-#     if client is None:
-#         await post(url, headers=headers, json=_json)
-#     else:
-#         async with client.post(url, headers=headers, json=_json) as resp:
-#             print(await resp.text())
-#             print(resp.status)
-
-
-# async def send_verify_email_token_to_user(
-#     *, email, token, name=None, client=None, verify_url=""
-# ):
-
-#     url = settings.EMAIL_SEND_URL
-#     key_secret = f"{settings.EMAIL_API_KEY}:{settings.EMAIL_API_SECRET}"
-#     auth = base64.b64encode(key_secret.encode()).decode()
-#     headers = {"Authorization": f"Basic {auth}"}
-
-#     _json = build_verify_email_token_json(
-#         email=email, token=token, name=name, url=verify_url
-#     )
-
-#     if client is None:
-#         await post(url, headers=headers, json=_json)
-#     else:
-#         async with client.post(url, headers=headers, json=_json) as resp:
-#             print(await resp.text())
-#             print(resp.status)
+            txt = await resp.text()
+            log = logging.warning if resp.status > 200 else logging.info
+            log(f"response status: {resp.status} text: {txt}")
