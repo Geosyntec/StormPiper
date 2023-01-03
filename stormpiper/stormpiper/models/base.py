6bf0047301a97721fc77f18d08bc0ab39908b5e6
Austin Orr <aorr@geosyntec.com>
2023-01-03 01:19:30 -0800

    lint

--- stormpiper/stormpiper/models/base.py
+++ stormpiper/stormpiper/models/base.py
@@ -6,6 +6,7 @@ class BaseModel(BASE):
     def get_fields(cls, by_alias=False):
         return list(cls.schema(by_alias=by_alias).get("properties", {}).keys())
 
+
 class BaseORM(BaseModel):
     class Config:
         orm_mode = True
