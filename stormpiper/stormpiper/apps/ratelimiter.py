ce04a816d66f0994605ddae47d9f191923ea85d2
Austin Orr <aorr@geosyntec.com>
2023-01-08 17:01:27 -0800

    implement rate limits for readonly access routes

---
+++ stormpiper/stormpiper/apps/ratelimiter.py
@@ -0,0 +1,27 @@
+from ipaddress import ip_address
+from typing import Tuple
+from random import randint
+
+from ratelimit.auths import EmptyInformation
+from ratelimit.types import Scope
+
+
+async def client_ip(scope: Scope) -> Tuple[str, str]:
+    """
+    parse ip
+    """
+    if scope["client"]:
+        ip, _ = tuple(scope["client"])
+        if any((s in str(ip) for s in ["testclient", "localhost"])):
+            return str(randint(1_000_000, 10_000_000 - 1)), "default"
+
+        if ip_address(ip).is_global:
+            return ip, "default"
+    else:
+
+        for name, value in scope["headers"]:
+            if name == b"x-real-ip":
+                ip = value.decode("utf8")
+                return ip, "default"
+
+    raise EmptyInformation(scope)
