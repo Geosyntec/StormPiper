f4f04930889e37a16adecac4a1b30f6b75f695a2
Austin Orr <aorr@geosyntec.com>
2023-01-11 22:47:18 -0800

    update user role model for json api spec

--- stormpiper/stormpiper/apps/supersafe/models.py
+++ stormpiper/stormpiper/apps/supersafe/models.py
@@ -1,26 +1,66 @@
 import uuid
 from enum import Enum
-from functools import total_ordering
-from typing import Optional
+from typing import Optional, Self
 
 from fastapi_users import schemas
 from pydantic import BaseModel
 
 
-@total_ordering
-class Role(Enum):
-    none = 0
-    public = 0  # alias for none;
-    reader = 20  # auth user; read only
-    user = 20  # auth user; read only; alias for reader; deprecated
-    editor = 40  # auth user; read/write
-    user_admin = 60  # grant rw or user_admin to other users
-    admin = 100  # systems admin; dev use only.
+class StrEnumMixin(str, Enum):
+    def _q(self):
+        return self.value
+
+    def __hash__(self) -> int:
+        return str(self.value).__hash__()
+
+    def __eq__(self, other):
+        if self.__class__ is other.__class__:
+            return self._q() == other._q()
+        return NotImplemented
+
+    def __ne__(self, other):
+        if self.__class__ is other.__class__:
+            return self._q() != other._q()
+        return NotImplemented
 
     def __le__(self, other):
         if self.__class__ is other.__class__:
-            return self.value <= other.value
+            return self._q() <= other._q()
         return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self._q() < other._q()
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self._q() >= other._q()
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self._q() > other._q()
+
+
+class Role(StrEnumMixin):
+    public = "public"  # alias for none;
+    none = "none"
+    reader = "reader"  # auth user; read only
+    user = "user"  # auth user; read only; alias for reader; deprecated
+    editor = "editor"  # auth user; read/write
+    user_admin = "user_admin"  # grant rw or user_admin to other users
+    admin = "admin"  # systems admin; dev use only.
+
+    def _q(self: Self) -> int:
+        _role_mapper: dict[str, int] = dict(
+            public=0,  # alias for none;
+            none=0,
+            reader=20,  # auth user; read only
+            user=20,  # auth user; read only; alias for reader; deprecated
+            editor=40,  # auth user; read/write
+            user_admin=60,  # grant rw or user_admin to other users
+            admin=100,  # systems admin; dev use only.
+        )
+        return _role_mapper[self.value]
 
 
 class UserExtras(BaseModel):
---
+++ stormpiper/stormpiper/tests/test_models.py
@@ -0,0 +1,22 @@
+import operator as op
+
+import pytest
+
+from stormpiper.apps.supersafe.models import Role
+
+
+@pytest.mark.parametrize(
+    "first, comparison, other",
+    [
+        (Role.admin, op.le, Role.admin),
+        (Role.public, op.le, Role.admin),
+        (Role.public, op.lt, Role.admin),
+        (Role.admin, op.ge, Role.admin),
+        (Role.admin, op.ge, Role.user),
+        (Role.admin, op.gt, Role.public),
+        (Role.public, op.eq, Role.none),
+        (Role.admin, op.eq, Role.admin),
+    ],
+)
+def test_role_ordering(first, comparison, other):
+    assert comparison(first, other)
