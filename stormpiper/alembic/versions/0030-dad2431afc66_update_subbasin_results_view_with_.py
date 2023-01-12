8bef44810748de7d7e7c556e3558261818c2b824
Austin Orr <aorr@geosyntec.com>
2023-01-12 00:08:48 -0800

    add conc to subbasin results and prom

---
+++ stormpiper/alembic/versions/0030-dad2431afc66_update_subbasin_results_view_with_.py
@@ -0,0 +1,102 @@
+"""update subbasin results view with concentrations
+
+Revision ID: dad2431afc66
+Revises: 95dce7183719
+Create Date: 2023-01-11 23:52:56.826551
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "dad2431afc66"
+down_revision = "95dce7183719"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        DROP VIEW IF EXISTS subbasinresult_v;
+        CREATE OR REPLACE VIEW subbasinresult_v AS
+        select
+            s."basinname",
+            s."subbasin",
+            s."area_acres",
+            s."access",
+            s."economic_value",
+            s."environmental_value",
+            s."livability_value",
+            s."opportunity_value",
+            s."geom",
+            sr."node_id",
+            sr."epoch",
+            sr."runoff_volume_cuft",
+            sr."TCu_load_lbs",
+            sr."TN_load_lbs",
+            sr."TP_load_lbs",
+            sr."TSS_load_lbs",
+            sr."TZn_load_lbs",
+            sr."PHE_load_lbs",
+            sr."PYR_load_lbs",
+            sr."DEHP_load_lbs",
+            sr."TCu_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "TCu_conc_mg/l",
+            sr."TN_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "TN_conc_mg/l",
+            sr."TP_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "TP_conc_mg/l",
+            sr."TSS_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "TSS_conc_mg/l",
+            sr."TZn_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "TZn_conc_mg/l",
+            sr."PHE_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "PHE_conc_mg/l",
+            sr."PYR_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "PYR_conc_mg/l",
+            sr."DEHP_load_lbs" / sr."runoff_volume_cuft" * 16018.46337 as "DEHP_conc_mg/l",
+            sr."runoff_volume_cuft" * 0.0002754821 / s.area_acres as "runoff_depth_inches",
+            sr."TCu_load_lbs" / s.area_acres as "TCu_yield_lbs_per_acre",
+            sr."TN_load_lbs" / s.area_acres as "TN_yield_lbs_per_acre",
+            sr."TP_load_lbs" / s.area_acres as "TP_yield_lbs_per_acre",
+            sr."TSS_load_lbs" / s.area_acres as "TSS_yield_lbs_per_acre",
+            sr."TZn_load_lbs" / s.area_acres as "TZn_yield_lbs_per_acre",
+            sr."PHE_load_lbs" / s.area_acres as "PHE_yield_lbs_per_acre",
+            sr."PYR_load_lbs" / s.area_acres as "PYR_yield_lbs_per_acre",
+            sr."DEHP_load_lbs" / s.area_acres as "DEHP_yield_lbs_per_acre"
+        from subbasin_result as sr JOIN subbasin as s on sr.subbasin = s.subbasin
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+        DROP VIEW IF EXISTS subbasinresult_v;
+        CREATE OR REPLACE VIEW subbasinresult_v AS
+        select
+            s."basinname",
+            s."subbasin",
+            s."area_acres",
+            s."access",
+            s."economic_value",
+            s."environmental_value",
+            s."livability_value",
+            s."opportunity_value",
+            s."geom",
+            sr."node_id",
+            sr."epoch",
+            sr."runoff_volume_cuft",
+            sr."TCu_load_lbs",
+            sr."TN_load_lbs",
+            sr."TP_load_lbs",
+            sr."TSS_load_lbs",
+            sr."TZn_load_lbs",
+            sr."PHE_load_lbs",
+            sr."PYR_load_lbs",
+            sr."DEHP_load_lbs",
+            sr."runoff_volume_cuft" * 0.0002754821 / s.area_acres as "runoff_depth_inches",
+            sr."TCu_load_lbs" / s.area_acres as "TCu_yield_lbs_per_acre",
+            sr."TN_load_lbs" / s.area_acres as "TN_yield_lbs_per_acre",
+            sr."TP_load_lbs" / s.area_acres as "TP_yield_lbs_per_acre",
+            sr."TSS_load_lbs" / s.area_acres as "TSS_yield_lbs_per_acre",
+            sr."TZn_load_lbs" / s.area_acres as "TZn_yield_lbs_per_acre",
+            sr."PHE_load_lbs" / s.area_acres as "PHE_yield_lbs_per_acre",
+            sr."PYR_load_lbs" / s.area_acres as "PYR_yield_lbs_per_acre",
+            sr."DEHP_load_lbs" / s.area_acres as "DEHP_yield_lbs_per_acre"
+        from subbasin_result as sr JOIN subbasin as s on sr.subbasin = s.subbasin
+        """
+    )
--- stormpiper/stormpiper/database/schemas/subbasin_result_view.py
+++ stormpiper/stormpiper/database/schemas/subbasin_result_view.py
@@ -15,12 +15,14 @@ sr_cols = [
 depth_col = "runoff_depth_inches"
 load_cols = [n for n in sr_cols if "_load_" in n]
 yield_cols = [n.replace("_load_lbs", "_yield_lbs_per_acre") for n in load_cols]
+conc_cols = [n.replace("_load_lbs", "_conc_mg/l") for n in load_cols]
 
 COLS = [
     *s_cols,
     *sr_cols,
     depth_col,
     *yield_cols,
+    *conc_cols,
 ]
 
 
@@ -30,7 +32,14 @@ def build_view_template():
     subr_cols = "\n".join([f"""\tsr."{s}",""" for s in sr_cols])
 
     cuft_per_acre_to_inch = 0.0002754821  # ac*in
+    lbs_per_cuft_to_mgl = 16018.46337  # lbs/cuft to mgl
     depth_col_calc = f"""\tsr."runoff_volume_cuft" * {cuft_per_acre_to_inch} / s.area_acres as "{depth_col}", """
+    conc_col_block = "\n".join(
+        [
+            f"""\tsr."{load_col}" / sr."runoff_volume_cuft" * {lbs_per_cuft_to_mgl} as "{conc_col}", """
+            for load_col, conc_col in zip(load_cols, conc_cols)
+        ]
+    )
     yield_col_calcs = [
         f"""\tsr."{load_col}" / s.area_acres as "{yield_col}", """
         for load_col, yield_col in zip(load_cols, yield_cols)
@@ -42,6 +51,7 @@ def build_view_template():
     view_template = f"""select
 {sub_cols}
 {subr_cols}
+{conc_col_block}
 {yield_col_block}
 from subbasin_result as sr JOIN subbasin as s on sr.subbasin = s.subbasin
 """
