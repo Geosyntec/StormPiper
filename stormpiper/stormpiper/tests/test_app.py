82a8f9ceb9710dcc9c6ea32cb9e440518026d09c
Austin Orr <aorr@geosyntec.com>
2023-01-16 20:22:33 -0800

    revert get user mod, add test to ensure ping works

--- stormpiper/stormpiper/tests/test_app.py
+++ stormpiper/stormpiper/tests/test_app.py
@@ -12,3 +12,17 @@ def test_frontend_app(client, route):
         response.content,
         response.status_code,
     )
+
+
+@pytest.mark.parametrize("client_name", ["client", "client_local"])
+def test_ping(client_lookup, client_name):
+    client = client_lookup[client_name]
+    response = client.get("/ping")
+    assert response.status_code == 200, (
+        response,
+        response.content,
+        response.status_code,
+    )
+    data = response.json()
+    # make sure api doesn't retutn an error json response
+    assert "user" in data, data
