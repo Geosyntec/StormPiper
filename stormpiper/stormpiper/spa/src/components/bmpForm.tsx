de06977921c8d269312fcd451b77c3fa25e8955c
acang-gs <aang@geosyntec.com>
2023-01-30 11:36:40 -0800

    added isDirty polling and results refresh

--- stormpiper/stormpiper/spa/src/components/bmpForm.tsx
+++ stormpiper/stormpiper/spa/src/components/bmpForm.tsx
@@ -255,15 +255,15 @@ export function BMPForm(props:formProps){
                                 }
                               })}
                             </TextField>
-                            :<TextField variant="outlined" margin="dense" {...register(formField.fieldID)} type = {formField.type} defaultValue={formField.value} required={formField.required} label={formField.label} disabled={formField.label==='Node Id'}/>
-                        }
+                            :<TextField variant="outlined" margin="dense" {...register(formField.fieldID)} type = {formField.type} defaultValue={formField.value} required={formField.required} label={formField.label} inputProps={{step:formField.type==='number'?0.01:null}} disabled={formField.label==='Node Id'}/>
+                          }
                     </div>
                 )
             })
             if (props.simpleFields) {
               simpleCheckDiv = (
                 <div className="simple-checkbox">
-                  <FormControlLabel control={<Switch defaultChecked onChange={() => setIsSimple(!isSimple)} color="primary"/>} label="Simple Facility?"/>
+                  <FormControlLabel control={<Switch checked={isSimple} onChange={() => setIsSimple(!isSimple)} color="primary"/>} label="Simple Facility?"/>
                 </div>
               );
             } else {
