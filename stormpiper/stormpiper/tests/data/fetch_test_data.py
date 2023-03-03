379ef17da694a52798cf63ccd4ce4724ae1013f3
Austin Orr <aorr@geosyntec.com>
2023-03-03 12:44:13 -0800

    add solved scenarios to test db seed script

--- stormpiper/stormpiper/tests/data/fetch_test_data.py
+++ stormpiper/stormpiper/tests/data/fetch_test_data.py
@@ -1,7 +1,10 @@
-import pandas
+import asyncio
+import json
 
 from stormpiper.connections import arcgis
-from stormpiper.src import loading
+from stormpiper.models.scenario import SCENARIO_EXAMPLES
+from stormpiper.models.scenario_validator import scenario_validator
+from stormpiper.src import loading, scenario
 from stormpiper.src.tmnt import default_tmnt_source_controls as src_ctrls
 from stormpiper.src.tmnt import spatial
 
@@ -27,14 +30,29 @@ def fetch_tacoma_gis_data():  # pragma: no cover
     lgus.to_file(datadir / "lgu_boundary.geojson", driver="GeoJSON")
 
     load = loading.compute_loading(lgu_boundary=lgus)
-    load.to_json(datadir / "lgu_load.json", orient="table")
+    load.to_json(datadir / "lgu_load.json", orient="table", indent=2)
 
     tmnt_src_ctrl = src_ctrls.dummy_tmnt_source_control(gdf_subbasin.subbasin.to_list())
-    tmnt_src_ctrl.to_json(datadir / "tmnt_source_control.json", orient="table")
+    tmnt_src_ctrl.to_json(
+        datadir / "tmnt_source_control.json", orient="table", indent=2
+    )
+
+
+def fetch_scenario_results():
+    scenarios = []
+    for _, dct in enumerate(SCENARIO_EXAMPLES.values()):
+        scenario_data = dct.get("value", {})
+        d = asyncio.run(scenario_validator(scenario_data)).dict(exclude_unset=True)
+        data = scenario.solve_scenario_data(data=d, force=True)
+        scenarios.append(data)
+
+    with open(datadir / "scenario.json", "w") as fp:
+        json.dump(scenarios, fp, indent=2, sort_keys=True, default=str)
 
 
 def main():  # pragma: no cover
     fetch_tacoma_gis_data()
+    fetch_scenario_results()
 
 
 if __name__ == "__main__":  # pragma: no cover
