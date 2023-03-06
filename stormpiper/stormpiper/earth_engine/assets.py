750ec0c50d8fda84f54a0286ad3b173785c8cce0
Austin Orr <aorr@geosyntec.com>
2023-03-06 12:57:06 -0800

    drop ee/assets test due to uselessness

--- stormpiper/stormpiper/earth_engine/assets.py
+++ stormpiper/stormpiper/earth_engine/assets.py
@@ -3,6 +3,6 @@ from stormpiper.core.config import settings
 from .ee import ee
 
 
-def assets():
+def assets():  # pragma: no cover
     project_folder = settings.EE_PROJECT_DIRECTORY
     return ee.data.listAssets({"parent": project_folder})
