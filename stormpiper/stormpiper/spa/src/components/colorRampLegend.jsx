b73bfccdcc9ee4aec2c80da9228a6ae39c05e1c4
Austin Orr <aorr@geosyntec.com>
2023-04-04 10:30:08 -0700

    prioritization page layout

--- stormpiper/stormpiper/spa/src/components/colorRampLegend.jsx
+++ stormpiper/stormpiper/spa/src/components/colorRampLegend.jsx
@@ -2,55 +2,62 @@ import { Box, Typography } from "@mui/material";
 
 export default function ColorRampLegend(props) {
   return (
-    <Box id="color-ramp-legend" sx={props.style}>
+    <Box id="color-ramp-legend" {...props}>
       <Box
-        style={{
-          top: "20%",
-          left: "5%",
-          width: "90%",
-          height: "20%",
-          border: "1 px solid black",
-          background:
-            "linear-gradient(to right, rgb(68,1,84) 0%, rgb(33,145,140) 50%, rgb(253, 231, 84) 100%)",
-          overflow: "hidden",
-          position: "absolute",
-        }}
-      ></Box>
-      <Box
-        style={{
-          top: "55%",
-          left: "15%",
-          overflow: "hidden",
-          position: "absolute",
+        sx={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
         }}
       >
-        <Typography align="center" variant="caption">
-          Subbasin Priority Score
-        </Typography>
-      </Box>
-      <Box
-        style={{
-          top: "45%",
-          left: "5%",
-          overflow: "hidden",
-          position: "absolute",
-        }}
-      >
-        <Typography align="center" variant="caption">
-          0
-        </Typography>
-      </Box>
-      <Box
-        style={{
-          top: "45%",
-          left: "85%",
-          overflow: "hidden",
-          position: "absolute",
-        }}
-      >
-        <Typography align="center" variant="caption">
-          100
-        </Typography>
+        <Box
+          sx={{
+            borderRadius: 0.5,
+            width: "100%",
+            height: "40%",
+            position: "absolute",
+            background: "#4a7d70",
+            backgroundImage:
+              "linear-gradient(90deg, #440154, #482475, #414487, #355f8d, #2a788e, #21908d, #22a884, #42be71, #7ad151, #bddf26,  rgb(253, 231, 84))",
+          }}
+        ></Box>
+        <Box sx={{ position: "absolute", top: "52%", width: "100%" }}>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="caption">Subbasin Priority Score</Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "10%",
+              left: "-5%",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="caption">0</Typography>
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              width: "10%",
+              right: "-5%",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="caption">100</Typography>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
