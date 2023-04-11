efa40df9d6709f7b12a275e44af8a36b09cce294
Austin Orr <aorr@geosyntec.com>
2023-04-11 14:49:57 -0700

    auth checker pattern

--- stormpiper/stormpiper/spa/src/components/authChecker.jsx
+++ stormpiper/stormpiper/spa/src/components/authChecker.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { UserProfileContext } from "./authProvider";
@@ -9,21 +9,25 @@ export default function AuthChecker({
   useNav,
   children,
 }) {
+  const [isBlocked, setIsBlocked] = useState(false);
   const userProfile = useContext(UserProfileContext);
   const navigate = useNavigate();
 
   const _allowedRoles = allowedRoles || [];
   const _disallowedRoles = disallowedRoles || [];
   const _useNav = useNav || false; // default to false
-  if (_allowedRoles.length && !_allowedRoles.includes(userProfile.role)) {
-    if (_useNav) navigate("/app");
-    return <></>;
-  }
 
-  if (_disallowedRoles.length && _disallowedRoles.includes(userProfile.role)) {
-    if (_useNav) navigate("/app");
-    return <></>;
-  }
+  useEffect(() => {
+    if (
+      _disallowedRoles.includes(userProfile.role) ||
+      !_allowedRoles.includes(userProfile.role)
+    ) {
+      if (_useNav) {
+        navigate("/app");
+      } else setIsBlocked(true);
+    }
+  }, [children]);
 
-  return <> {children} </>;
+  if (isBlocked) return <></>;
+  return <>{children}</>;
 }
