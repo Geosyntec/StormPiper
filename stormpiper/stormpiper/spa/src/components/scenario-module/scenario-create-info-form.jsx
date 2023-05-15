2b93d5e41ab8810a7605895a72509f1e6f7b1154
Adrian Ang <74433074+acang-gs@users.noreply.github.com>
2023-05-15 12:51:30 -0700

    remove scen info form code

--- stormpiper/stormpiper/spa/src/components/scenario-module/scenario-create-info-form.jsx
+++ stormpiper/stormpiper/spa/src/components/scenario-module/scenario-create-info-form.jsx
@@ -59,17 +59,7 @@ export const ScenarioInfoForm = forwardRef(function ScenarioInfoForm(
     () => {
       return {
         async triggerValidation() {
-          console.log("Scenario info values: ", getValues());
           const formValidation = await trigger();
-          console.log("Scenario info form isValid: ", formValidation);
-          console.log("Scenario info form errors: ", Object.keys(errors));
-          // const noErrors = () => {
-          //   if (Object.keys(errors).length) {
-          //     return Object.keys(errors).length === 0;
-          //   } else {
-          //     return true;
-          //   }
-          // };
           const formValid = formValidation;
           return formValid;
         },
@@ -79,7 +69,6 @@ export const ScenarioInfoForm = forwardRef(function ScenarioInfoForm(
   );
 
   function _renderFormFields() {
-    console.log("Building scenario info fields: ", fields);
     let fieldDiv = Object.values(fields).map((formField) => {
       return (
         <Box key={formField.fieldID} sx={{ width: "100%" }}>
@@ -93,7 +82,6 @@ export const ScenarioInfoForm = forwardRef(function ScenarioInfoForm(
               required={formField.required}
               margin="dense"
               onChange={(e) => {
-                console.log("Form errors: ", errors);
                 scenarioSetter(formField.fieldID, e.target.value);
               }}
               {...formField.inputProps}
@@ -114,44 +102,10 @@ export const ScenarioInfoForm = forwardRef(function ScenarioInfoForm(
               {errors[formField.fieldID]?.message}
             </Typography>
           )}
-          {/* <TextField
-            key={formField.fieldID}
-            label="there"
-            defaultValue="hello"
-          ></TextField> */}
         </Box>
       );
     });
     return fieldDiv;
-  }
-
-  async function _handleSubmit(data) {
-    const formData = {
-      ...scenario,
-      name: data.name,
-      info: { purpose: data.purpose, description: data.description },
-    };
-    console.log("Submitting scenario: ", formData);
-    // const response = await api_fetch("/api/rest/scenario", {
-    //   credentials: "same-origin",
-    //   headers: {
-    //     accept: "application/json",
-    //     "Content-type": "application/json",
-    //   },
-    //   method: "POST",
-    //   body: JSON.stringify(formData),
-    // }).then((resp) => {
-    //   if (resp.status == 200) {
-    //     console.log("redirect on success");
-    //     setSuccess(true);
-    //     setError(false);
-    //   } else {
-    //     console.warn("login failure", resp);
-    //     setError(true);
-    //   }
-    // });
-
-    return response;
   }
 
   return (
@@ -163,38 +117,12 @@ export const ScenarioInfoForm = forwardRef(function ScenarioInfoForm(
       }}
     >
       <Box
-        // component="form"
-        // noValidate
-        // autoComplete="off"
+        component="form"
+        noValidate
+        autoComplete="off"
         sx={{ width: "100%" }}
-        // onSubmit={handleSubmit(_handleSubmit)}
       >
-        <form onSubmit={handleSubmit(_handleSubmit)}>
-          {_renderFormFields()}
-        </form>
-
-        {/* {errors && (
-            <Box sx={{ my: "2rem", display: "flex", justifyContent: "start" }}>
-              <Typography
-                variant="caption"
-                align="left"
-                color={(theme) => theme.palette.warning.main}
-              >
-                Something went wrong - please try again
-              </Typography>
-            </Box>
-          )}
-          {success && (
-            <Box sx={{ my: "2rem", display: "flex", justifyContent: "start" }}>
-              <Typography
-                variant="caption"
-                align="left"
-                color={(theme) => theme.palette.success.main}
-              >
-                Scenario created successfully
-              </Typography>
-            </Box>
-          )} */}
+        {_renderFormFields()}
       </Box>
     </Box>
   );
