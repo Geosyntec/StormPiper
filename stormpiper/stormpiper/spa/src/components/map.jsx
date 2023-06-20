e6de1f5c252636427e190fe9e8d86e5bfe336209
Austin Orr <aorr@geosyntec.com>
2023-06-20 15:59:22 -0700

    fix zoom behavior

--- stormpiper/stormpiper/spa/src/components/map.jsx
+++ stormpiper/stormpiper/spa/src/components/map.jsx
@@ -46,9 +46,6 @@ const tooltipFieldDict = {
   default: [{ id: "altid", label: "ID" }],
 };
 
-const LONGITUDE_RANGE = [-122.7, -121.8];
-const LATITUDE_RANGE = [47.05, 47.45];
-
 function getTooltipContents(object, layerLabel, fields) {
   const feat = object;
   if (feat) {
@@ -73,7 +70,6 @@ function DeckGLMap({
   currentFeatureID,
   zoomID,
   zoomFeature,
-  setZoomLevel,
   tooltipObj,
   ...props
 }) {
@@ -146,22 +142,6 @@ function DeckGLMap({
       controller={{ doubleClickZoom: false }}
       layers={props.layers}
       onHover={({ object }) => (isHovering = Boolean(object))}
-      onViewStateChange={({ viewState, oldViewState }) => {
-        viewState.longitude = Math.min(
-          LONGITUDE_RANGE[1],
-          Math.max(LONGITUDE_RANGE[0], viewState.longitude)
-        );
-        viewState.latitude = Math.min(
-          LATITUDE_RANGE[1],
-          Math.max(LATITUDE_RANGE[0], viewState.latitude)
-        );
-        if (viewState.zoom < 9) {
-          viewState = { ...oldViewState, zoom: 9 };
-        }
-        setZoomLevel && setZoomLevel(viewState.zoom);
-
-        return viewState;
-      }}
       getCursor={({ isDragging }) =>
         isDragging ? "grabbing" : isHovering ? "pointer" : "grab"
       }
