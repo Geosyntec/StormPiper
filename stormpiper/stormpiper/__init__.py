aed88cd68cf6fb8e1127de2dc59eed5058f00ec1
Austin Orr <aorr@geosyntec.com>
2023-06-26 16:12:57 -0700

    add copyright to license and main init file

--- stormpiper/stormpiper/__init__.py
+++ stormpiper/stormpiper/__init__.py
@@ -1,3 +1,9 @@
+"""Platform for modeling and prioritizing stormwater facilities the city of Tacoma, WA.
+
+Copyright (c) 2023, Geosyntec Consultants Inc.
+All rights reserved.
+"""
+
 __version__ = "0.3.1"
 __author__ = "Austin Orr"
 __email__ = "aorr@geosyntec.com"
