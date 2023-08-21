69886dc66023c94a60cbcdea39bed17b8ecf10cf
Austin Orr <aorr@geosyntec.com>
2023-08-21 15:11:41 -0700

    add Ecology, Tacooma, and Geosyntec logos closes #292

--- stormpiper/stormpiper/spa/src/components/landing.jsx
+++ stormpiper/stormpiper/spa/src/components/landing.jsx
@@ -15,8 +15,12 @@ import MapIcon from "@mui/icons-material/Map";
 import HubIcon from "@mui/icons-material/Hub";
 
 import { UserProfileContext } from "./authProvider";
+import { FullSpan } from "./base/two-col-grid";
 
 import heroUrl from "../assets/img/hero-stormdrain.jpg";
+import ecology_logo from "../assets/img/ecylogo-wide-color.svg";
+import tacoma_logo from "../assets/img/Tacoma-Logo-3.png";
+import geosyntec_logo from "../assets/img/geosyntec-logo.png";
 
 function ActionAreaCard({ img, header, content, ...props }) {
   return (
@@ -108,146 +112,179 @@ export default function Landing() {
       <Box
         sx={{
           position: "absolute",
-          width: "60%",
           top: "85%",
-          left: "20%",
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <Grid
-          container
-          rowSpacing={2}
-          columnSpacing={2}
-          alignItems="stretch"
-          justifyContent="flex-start"
+        <FullSpan
+          sx={{
+            width: { xs: "100%", md: "700px" },
+            px: 2,
+          }}
         >
           <Grid
-            item
-            xs={12}
-            md={6}
-            component={Link}
-            to={allowNav && "/app/map"}
-            sx={{
-              textDecoration: "none",
-              display: "flex",
-              justifyContent: "center",
-            }}
+            container
+            rowSpacing={2}
+            columnSpacing={2}
+            alignItems="stretch"
+            justifyContent="flex-start"
           >
-            <ActionAreaCard
-              img={
-                <MapIcon
-                  color="primary"
-                  fontSize="large"
-                  sx={{ transform: "scale(1.5)" }}
-                />
-              }
-              header={"Map Explorer"}
-              content={`
+            <Grid
+              item
+              xs={12}
+              md={6}
+              component={Link}
+              to={allowNav && "/app/map"}
+              sx={{
+                textDecoration: "none",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <ActionAreaCard
+                img={
+                  <MapIcon
+                    color="primary"
+                    fontSize="large"
+                    sx={{ transform: "scale(1.5)" }}
+                  />
+                }
+                header={"Map Explorer"}
+                content={`
                     Visualize the existing state of the stormwater BMP system. Search for specific facilities, and explore subbasins, pollutant heat maps, and reference imagery.
                     `}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            component={Link}
-            to={allowNav && "/app/view-results"}
-            sx={{
-              textDecoration: "none",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <ActionAreaCard
-              img={
-                <GridOnRoundedIcon
-                  color="primary"
-                  fontSize="large"
-                  sx={{ transform: "scale(1.5)" }}
-                />
-              }
-              header={"WQ Results Viewer"}
-              content={`
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              component={Link}
+              to={allowNav && "/app/view-results"}
+              sx={{
+                textDecoration: "none",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <ActionAreaCard
+                img={
+                  <GridOnRoundedIcon
+                    color="primary"
+                    fontSize="large"
+                    sx={{ transform: "scale(1.5)" }}
+                  />
+                }
+                header={"WQ Results Viewer"}
+                content={`
               Evaluate BMP performance, pinpoint potential retrofit
               sites, identify viable approaches to treat stormwater
               and improve Tacoma’s receiving waters.
                     `}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            component={Link}
-            to={allowNav && "/app/prioritization"}
-            sx={{
-              textDecoration: "none",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <ActionAreaCard
-              img={
-                <CompareArrowsIcon
-                  fontSize="large"
-                  color="primary"
-                  sx={{
-                    transform: "rotate(90deg) scale(1.5)",
-                  }}
-                />
-              }
-              header={"Decision Support"}
-              content={`
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              component={Link}
+              to={allowNav && "/app/prioritization"}
+              sx={{
+                textDecoration: "none",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <ActionAreaCard
+                img={
+                  <CompareArrowsIcon
+                    fontSize="large"
+                    color="primary"
+                    sx={{
+                      transform: "rotate(90deg) scale(1.5)",
+                    }}
+                  />
+                }
+                header={"Decision Support"}
+                content={`
                   Prioritize investments and allocate resources more effectively
                   through an understanding of life-cycle costs and project benefits.
                   `}
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            component={Link}
-            to={allowNav && "/app/scenario"}
-            sx={{
-              textDecoration: "none",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <ActionAreaCard
-              img={
-                <HubIcon
-                  fontSize="large"
-                  color="primary"
-                  sx={{
-                    transform: "scale(1.5)",
-                  }}
-                />
-              }
-              header={"Scenario Builder"}
-              content={`
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              component={Link}
+              to={allowNav && "/app/scenario"}
+              sx={{
+                textDecoration: "none",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <ActionAreaCard
+                img={
+                  <HubIcon
+                    fontSize="large"
+                    color="primary"
+                    sx={{
+                      transform: "scale(1.5)",
+                    }}
+                  />
+                }
+                header={"Scenario Builder"}
+                content={`
                   Ensure decisions help improve watershed conditions for all
                   community members. Help promote equitable and sustainable
                   outcomes in stormwater project and enhance neighborhoods for everybody.
 
                     `}
-            />
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100px",
-            width: "100%",
-          }}
-        >
-          <Typography sx={{ color: "grey" }} variant="caption" align="center">
-            Copyright © 2023 Geosyntec Consultants Inc.
-          </Typography>
-        </Box>
+          <Grid container sx={{ py: 2, alignItems: "center" }}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <img src={ecology_logo} height="100px" alt="Ecology Logo" />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <img src={tacoma_logo} height="80px" alt="Tacoma Logo" />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <img src={geosyntec_logo} height="100px" alt="Geosyntec Logo" />
+            </Grid>
+          </Grid>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100px",
+              width: "100%",
+            }}
+          >
+            <Typography sx={{ color: "grey" }} variant="caption" align="center">
+              Copyright © 2023 Geosyntec Consultants Inc.
+            </Typography>
+          </Box>
+        </FullSpan>
       </Box>
     </Box>
   );
