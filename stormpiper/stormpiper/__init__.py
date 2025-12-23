0e00c4dae7898c35931c781d88e2888643dc376c
Austin Orr <austinorr@users.noreply.github.com>
2025-12-23 12:34:27 -0800

    Bump version to 1.1.1

--- stormpiper/stormpiper/__init__.py
+++ stormpiper/stormpiper/__init__.py
@@ -4,7 +4,7 @@ Copyright (c) 2023, Geosyntec Consultants Inc.
 All rights reserved.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "Austin Orr"
 __email__ = "aorr@geosyntec.com"
 
