c269133c43c4021165e909f04724ddc7e32add17
Austin Orr <aorr@geosyntec.com>
2026-01-08 12:06:04 -0800

    bump deps

--- scripts/bump_deps.sh
+++ scripts/bump_deps.sh
@@ -5,8 +5,8 @@ set -e
 pip install -U "uv>=0.9,<0.10"
 
 # dev
-uv pip compile stormpiper/requirements_unpinned.txt > stormpiper/requirements.txt
+uv pip compile requirements/requirements_unpinned.txt > requirements/requirements.txt
 
 # nereid
-uv pip compile stormpiper/requirements_test_unpinned.txt > stormpiper/requirements_test.txt
+uv pip compile requirements/requirements_test_unpinned.txt > requirements/requirements_test.txt
 
